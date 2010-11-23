3471cf1b50312fec89647944c8fff6554f59b0c6
Morgan Gustavsson <morgan@interaktivutbildning.net>
2010-11-23 22:04:52 +0100

    Layout & routes done + default index removed

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
+
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
@@ -20,4 +21,10 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+  
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+  
 end
