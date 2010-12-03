bd06d1a7b830efec6f1675ff5f939dc7918f39a3
Morgan Gustavsson <morgan@interaktivutbildning.net>
2010-12-03 20:30:53 +0100

    Done with sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 SampleApp::Application.routes.draw do
+  
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   
   #get "users/new" # Made obsolete after the above line resources :users
-
-  match '/signup', :to => 'users#new'
+  #get "sessions/new" # See above
+  
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
